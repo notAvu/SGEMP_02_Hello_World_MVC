63a3aaa4c10dc13a478b9297b58fd76342014fa6
Álvaro <alvaro.fernandez@iesnervion.es>
2021-10-19 13:48:13 +0200

    Agregar archivos de proyecto.

---
+++ 02_Hello_World_MVC_UI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_Hello_World_MVC_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Hello"] = "Bienvenido compañero";
+            return View();
+        }
+        public string Avu() 
+        {
+            return "Avu";
+        }
+    }
+}
